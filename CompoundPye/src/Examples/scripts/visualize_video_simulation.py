6e2849430d076ab89b39dff995668e6a28c590e0
ilyas <ilyasp.ku@gmail.com>
2016-11-10 12:10:19 +0100

    visualization of T4/T5 quiver plot almost working now

--- CompoundPye/src/Examples/scripts/visualize_video_simulation.py
+++ CompoundPye/src/Examples/scripts/visualize_video_simulation.py
@@ -1,15 +1,15 @@
 
 
-import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 
 DATA_LOCATION_PREFIX = "/media/windows/ilyas/data/360video/apis/0s_to_5s/"
-DATA_LOCATION_PREFIX = "/home/ilyas/Data/CompoundPye/360video/apis/0s_to_0.1s/"
+DATA_LOCATION_PREFIX = "/home/ilyas/Data/CompoundPye/360video/apis/0s_to_1s/"
 
 component_positions = np.load(DATA_LOCATION_PREFIX+"component_positions.npy")
 component_directions = np.load(DATA_LOCATION_PREFIX+ "component_directions.npy")
 component_labels = np.load(DATA_LOCATION_PREFIX + "component_labels.npy")
+
 
 #######################################################
 # > determine positions of columns                    #
@@ -104,19 +104,72 @@ def compute_arrows(current_neuron_outputs, ## current readings from T4 and T5 ce
 
     return node_arrows
 
+def direction_to_color(node_arrows):
+    
+    C = np.zeros((node_arrows.shape[0],3))
+
+    angles = np.arctan2(node_arrows[:,0], node_arrows[:,1])
+
+    C[:,0] = np.cos(angles)
+    C[:,1] = np.sin(angles)
+
+    C = C * 255.0
+
+    return C
+
+
+#######################################################
+# >> window-filter output to roughly match the frames #
+#   per second of the video                           #
+#######################################################
+
+
+## throw away at least the first
+# 100 crappy values ...
+START_INDEX = 100
+
+components_output = np.load(DATA_LOCATION_PREFIX + "components.npy")[START_INDEX:,:]
+
+t = np.load(DATA_LOCATION_PREFIX + "t.npy")[START_INDEX:]
+
+
+cap = cv2.VideoCapture("/home/ilyas/UNI/CompoundPye/Videos/market_360_degree_gray.avi")
+
+FPS = cap.get(cv2.CAP_PROP_FPS)
+DT = t[1]-t[0]
+T_END = t[-1]
+
+#DT_PER_FRAMES = 1./FPS/DT
+
+t_frames = np.arange(t[0], T_END, 1./FPS)
+DT_frames = 1./FPS
+
+filtered_output = np.zeros((t_frames.shape[0]-1, components_output.shape[1]))
+
+for i in range(t_frames.shape[0] - 1):
+    t_begin = t_frames[i]
+    t_end = t_frames[i+1]
+
+    data_indices = np.where((t >= t_begin) & (t < t_end))[0]
+
+    filtered_output[i] = components_output[data_indices,:].mean(axis = 0)
+
+    
+
 #######################################################
 # > create animation                                  #
 #######################################################
 
-                                                                    ## throw away at least the first
-                                                                    # 100 crappy values ...
-components_output = np.load(DATA_LOCATION_PREFIX + "components.npy")[100:,:]
-t = np.load(DATA_LOCATION_PREFIX + "t.npy")
+
+import matplotlib
+matplotlib.use('Agg')
+
+
+import matplotlib.pyplot as plt
+
 
 f,ax = plt.subplots(1,2, sharey = True)
 ax[0].set_xlim(0.0,1.0)
-
-cap = cv2.VideoCapture("/home/ilyas/UNI/CompoundPye/Videos/market_360_degree_gray.avi")
 
 
 ## for now only static for testing, no animations ...
@@ -128,22 +181,47 @@ Q = ax[0].quiver(node_positions[:,0], node_positions[:,1], node_arrows[:,0]/node
 
 I = ax[1].imshow(np.random.rand(960,1920), cmap = "gray", extent = (0.0,1.0,0.0,1.0))
 
-I.set_data(cap.read()[1])
+ret, frame = cap.read()
+
+#I.set_data(frame)
 
 def update_plot(num, Q, I, components_output):
+
+    global frame
+    
+    t_num = t_frames[num]
+    
     print num
+    
     node_arrows = compute_arrows(components_output[num,:], nodes)
-    Q.set_UVC(node_arrows[:,0]/node_arrows[:,0].max()*0.1,node_arrows[:,1]/node_arrows[:,1].max()*0.1)
+    C = direction_to_color(node_arrows)
+    Q.set_UVC(node_arrows[:,0]/node_arrows[:,0].max()*0.1,
+              node_arrows[:,1]/node_arrows[:,1].max()*0.1)
+              #C) ## color, not figured out yet
+
+    while cap.get(cv2.CAP_PROP_POS_MSEC) < t_num*1000:
+        ret, frame = cap.read()
+
+    I.set_data(frame)
 
     return Q, I
 
+
+
+
 from matplotlib import animation
 
-anim = animation.FuncAnimation(f, update_plot, frames = 100,
-                               fargs = (Q, I, components_output), interval = 10,
-                               blit = False)    
+anim = animation.FuncAnimation(f, update_plot, frames = 41, fargs = (Q, I, filtered_output), interval = 10)#, blit = False)
 
-f.show()
+#x = np.linspace(0, 2*np.pi, 200)
+#init_animation()
+
+#anim = animation.FuncAnimation(f, animate, init_func = init_animation,frames = 50, fargs = (), interval = 10)#, blit = False)    
 
 
+#f.show()
 
+#anim.save("~/UNI/CompoundPye/tmp/animation.mp4", writer = mywriter)
+anim.save("/home/ilyas/UNI/CompoundPye/tmp/animation.gif", writer = "imagemagick", fps = 10)
+
+#f.show()
